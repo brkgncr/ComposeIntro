5a68e5b2b67dbb823d6a0df507244d7c7153e27b
Burak Gençer <bggithub@proton.me>
2025-02-27 23:13:13 +0300

    Test 2

--- app/src/main/java/com/burak/composeintro/MainActivity.kt
+++ app/src/main/java/com/burak/composeintro/MainActivity.kt
@@ -4,13 +4,26 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.TextUnit
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.burak.composeintro.ui.theme.ComposeIntroTheme
 
 class MainActivity : ComponentActivity() {
@@ -20,41 +33,80 @@ class MainActivity : ComponentActivity() {
         setContent {
             ComposeIntroTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                    Box(modifier = Modifier.padding(innerPadding)) {
+                        MainScreen()
+                    }
                 }
             }
         }
-
-        lambdaTestFunction {
-            testFunction()
-        }
-
-    }
-
-    fun lambdaTestFunction(int: Int = 5, myFunction: () -> Unit) {
-        myFunction.invoke()
-    }
-
-    fun testFunction() {
-        println("test")
     }
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun MainScreen() {
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(Color.LightGray),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Text(
+            text = "Hello Compose",
+            color = Color.Black,
+            fontWeight = FontWeight.Bold,
+            fontSize = 56.sp
+        )
+
+        Spacer(modifier = Modifier.padding(20.dp))
+
+
+        Text(
+            text = "Hello World",
+            color = Color.Red,
+            fontWeight = FontWeight.Bold,
+            fontSize = 56.sp
+        )
+
+        Spacer(modifier = Modifier.padding(60.dp))
+
+        Text(
+            text = "Hello Compose",
+            color = Color.Black,
+            fontWeight = FontWeight.Bold,
+            fontSize = 56.sp
+        )
+
+        Spacer(modifier = Modifier.padding(20.dp))
+
+
+        Text(
+            text = "Hello World",
+            color = Color.Red,
+            fontWeight = FontWeight.Bold,
+            fontSize = 56.sp
+        )
+
+        Row(
+            modifier = Modifier
+                .fillMaxWidth()
+                .background(Color.Red),
+            horizontalArrangement = Arrangement.Center,
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            Text(text ="Row Element 1", color = Color.White)
+            Text(text ="Row Element 1", color = Color.White)
+        }
+
+    }
+
 }
+
 
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
     ComposeIntroTheme {
-        Greeting("Android")
+        MainScreen()
     }
 }
